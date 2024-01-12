030b298bfc063386435cf6b768e4581a4cbc83fb
Poochit Somwang <a677010@AR8040677010N1.local>
2024-01-12 13:45:06 +0700

    fix acme-logo

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -3,12 +3,9 @@ import { Lusitana } from "next/font/google";
 
 const lusitana = Lusitana( {weight:["400","700"]})
 
-
 export default function AcmeLogo() {
   return (
-    <div
-      className="flex flex-row items-center leading-none text-white"
-    >
+    <div className="flex flex-row items-center leading-none text-white">
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className={`${lusitana.className} text-[44px]`}>Acme</p>
     </div>
